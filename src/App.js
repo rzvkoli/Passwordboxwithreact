bc42a71fdb954c5a4a4d8c62991e8e8c0672fe49
Reza Tavakoli <rzvkoli@gmail.com>
2022-07-19 23:10:29 +0430

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React , {useState} from 'react';
+import { Icon } from 'react-icons-kit';
+import {eye} from 'react-icons-kit/feather/eye';
+import {eyeOff} from 'react-icons-kit/feather/eyeOff';
+
+function App() {
+
+  const [type , setType] = useState('password');
+  const [icon , setIcon] = useState(eyeOff);
+
+  const handleClick = () => {
+    if(type === 'password'){
+      setType('text');
+      setIcon(eyeOff)
+    }else{
+      setType('password');
+      setIcon(eye)
+    }
+  }
+  
+  return (
+      <div className = "wrapper">
+        <div className='input-fields'>
+            <span onClick = {handleClick} ><Icon icon={icon} size={25} /></span>
+            <input type={type} placeholder = 'Password' />
+        </div>
+      </div>
+  );
+}
+
+export default App;
